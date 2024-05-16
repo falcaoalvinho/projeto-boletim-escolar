2f765c60d1b32ce5a99ec15715b4ef9dfd21cf17
Álvaro José <josealvaro3421@gmail.com>
2024-05-16 15:33:17 -0300

    Correções na folha de estilo, mudança nos valores de texto do botão 'Adicionar Notas' e Instanciamneto das funções dos Botões

---
+++ source/script.js
@@ -0,0 +1,15 @@
+function ExcluirAluno() {
+    console.log("ExcluirAluno() foi chamada!")
+}
+
+function VerificarNotas() {
+    console.log("VerificarNotas() foi chamada!")
+}
+
+function GerarTabela() {
+    console.log("GerarTabela() foi chamada!")
+}
+
+function AdicionarAluno() {
+    console.log("AdicionarAluno() foi chamada!")
+}
